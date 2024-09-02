80c77e94a608f4d161ed9c08edfef3b1c4cb348d
Rongjun GENG <rj.geng@gmail.com>
2024-09-02 16:27:13 -0700

    add simple module exports test

--- car/__test__/car.spec.js
+++ car/__test__/car.spec.js
@@ -1,7 +1,13 @@
 // Test away!
 
+const sum = require('../car');
+
 describe('intro to testing', () => { 
   it('[1] sanity', () => {
     expect(true).not.toBe(false);
   })
+
+  it('[2] test: adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 })
--- car/car.js
+++ car/car.js
@@ -1,2 +1,6 @@
 // Build a Car class! - 
 
+function sum(a, b) {
+    return a + b;
+  }
+module.exports = sum;
