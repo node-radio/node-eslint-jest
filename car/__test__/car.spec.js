7cb3de32e00d18e237772150a1edf65c77d6b021
Rongjun GENG <rj.geng@gmail.com>
2024-09-03 07:42:02 -0700

    add TDD todo list for project Car

--- car/__test__/car.spec.js
+++ car/__test__/car.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 
-const sum = require('../car');
+const { sum, Car } = require('../car');
 
 describe('intro to testing', () => { 
   it('[1] sanity', () => {
@@ -10,4 +10,19 @@ describe('intro to testing', () => {
   it('[2] test: adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+  
+})
+
+describe('Car class', () => {
+  let prius
+  beforeEach(() => {
+    prius = new Car('prius', 'toyota')
+  });
+
+  it.todo('[3] exists');
+  it.todo('[4] has a model prop prius');
+  it.todo('[5] has a make prop totota');
+  it.todo('[6] has an odometer at 0 miles');
+  it.todo('[7] has a drive method that takes distance and inc odometer');
+  it.todo('[8] more todo list');
 })
--- car/car.js
+++ car/car.js
@@ -3,4 +3,12 @@
 function sum(a, b) {
     return a + b;
   }
-module.exports = sum;
+
+  class Car {
+
+  }
+  
+  module.exports = {
+    sum,
+    Car
+  };
